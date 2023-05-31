66f402842583b9a030b2a35b37b860d6aca6fc80
Kim Minjoo <roses16.dev@gmail.com>
2023-05-31 16:12:55 +0900

    edit/delete 1 function

--- programmers/level_2/12981.py
+++ programmers/level_2/12981.py
@@ -2,19 +2,9 @@
 # 영어 끝말잇기
 
 def solution(n, words):
-    checked_words = [words[0]]
-    words = words[1:]
-
-    for word in words:
-        if word[0] != checked_words[-1][-1]:
-          break
-
-        if checked_words.count(word) >= 1:
-           break
-        
-        checked_words.append(word)
-
-    if len(words) + 1 == len(checked_words):
-       return [0, 0]
-    else:
-       return [len(checked_words) % n + 1, len(checked_words) // n + 1]
+    for index in range(1, len(words)):
+       if words[index-1][-1] != words[index][0] or words[index] in words[:index]:
+          return [index % n + 1 , index // n + 1]
+       
+    return [0, 0]
+  
