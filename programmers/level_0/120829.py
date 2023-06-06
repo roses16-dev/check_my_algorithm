610b53b7c896cfbf7d5ea439586bc256bf121082
Kim Minjoo <roses16.dev@gmail.com>
2023-06-06 16:07:53 +0900

    add 2 test

---
+++ programmers/level_0/120829.py
@@ -0,0 +1,6 @@
+# 2023.06.06
+# 각도기
+
+def solution(angle):
+
+    return angle // 90 + ( 2 if angle > 90 else 1 )
---
+++ programmers/level_0/181862.py
@@ -0,0 +1,16 @@
+# 2023.06.06
+# 세 개의 구분자
+
+def solution(myStr):
+    answer = []
+
+    answer = myStr.split('a')
+
+    for str in answer:
+        str.split('b')
+        
+    return answer
+
+
+
+print(solution("baconlettucetomato"	))
