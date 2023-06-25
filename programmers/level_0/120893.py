edf279ff423fce0fd76dc3f9d5ce8ae95da1bf23
Kim Minjoo <roses16.dev@gmail.com>
2023-06-25 10:02:24 +0900

    add tests

---
+++ programmers/level_0/120893.py
@@ -0,0 +1,8 @@
+# 2023.06.25
+# 대문자와 소문자
+
+def solution(my_string):
+    return my_string.swapcase()
+
+
+
---
+++ programmers/level_0/181853.py
@@ -0,0 +1,5 @@
+# 2023.06.25
+# 뒤에서 5등까지
+
+def solution(num_list):
+    return sorted(num_list)[:5]
