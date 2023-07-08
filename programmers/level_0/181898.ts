564d25eff423ba74c8c80251e95358cd1f07cdb2
Kim Minjoo <roses16.dev@gmail.com>
2023-07-09 00:24:18 +0900

    add test

---
+++ programmers/level_0/181898.ts
@@ -0,0 +1,6 @@
+// 2023.07.09
+// 가까운 1 찾기
+
+function solution(arr: number[], idx: number): number {
+  return arr.indexOf(1, idx);
+}
---
+++ ts/181898.js
@@ -0,0 +1,5 @@
+// 2023.07.09
+// 가까운 1 찾기
+function solution(arr, idx) {
+    return arr.indexOf(1, idx);
+}
