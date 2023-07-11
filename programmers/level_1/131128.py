5e7d6424a9bbcfac14cfcd2abeb11c79e7cd797e
Kim Minjoo <roses16.dev@gmail.com>
2023-07-11 22:26:27 +0900

    solve

--- programmers/level_1/131128.py
+++ programmers/level_1/131128.py
@@ -1,23 +1,16 @@
-# 2023.07.10
+# 2023.07.10~11
 # 숫자 짝꿍
-# 푸는 중인 문제, 11~15번 케이스 시간초과 실패
 
 def solution(X, Y):
-    arr_z = []
-    arr_x = list(X)
-    arr_y = list(Y)
+    answer = ''
 
-    for num in arr_x:
-        if num in arr_y:
-            arr_z.append(num)
-            arr_y.remove(num)
+    for num in range(9, -1, -1):
+        answer += str(num) * min(X.count(str(num)), Y.count(str(num)))
 
-    if len(arr_z) == 0:
+    if len(answer) == 0:
         return "-1"
     
-    if len(arr_z) == arr_z.count("0"):
+    if len(answer) == answer.count("0"):
         return "0"
     
-    return ''.join(sorted(arr_z, reverse=True))
-
-print(solution("100",	"2345"))
+    return answer
